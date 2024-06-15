020bf98a744c4d799bb0d3473cdc7a68ea06c9ef
kamil-cs50 <kamiltukendorf2002@gmail.com>
2024-06-15 16:51:17 +0200

    Initial commit

---
+++ bot.js
@@ -0,0 +1,49 @@
+require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+const TelegramBot = require('node-telegram-bot-api');
+
+// Initialize the express application
+const app = express();
+
+// This token you have in your environment variables
+const token = process.env.TELEGRAM_BOT_TOKEN;
+const gifUrl = process.env.GIF_URL;
+const websiteUrl = process.env.WEBSITE_URL;
+
+// Create a bot that uses 'webhook' to fetch new updates
+const bot = new TelegramBot(token);
+
+// Parse the updates to JSON
+app.use(bodyParser.json());
+
+// Define the endpoint that is used by Telegram webhook
+app.post('/', (req, res) => {
+    bot.processUpdate(req.body);
+    res.sendStatus(200);
+});
+
+// Listen for messages from users 
+bot.on('message', async (msg) => {
+    const chatId = msg.chat.id;
+
+    if (msg.text === '/start') {
+        // Send the GIF
+        await bot.sendDocument(chatId, gifUrl);
+
+        // Send the message with the PLAY button
+        const message = await bot.sendMessage(chatId, "Play the game to earn points! 🔥", {
+            reply_markup: {
+                inline_keyboard: [[{
+                    text: "PLAY 🎮",
+                    web_app: { url: websiteUrl }
+                }]]
+            }
+        });
+
+        // Pin the text message
+        await bot.pinChatMessage(chatId, message.message_id, { disable_notification: true });
+    }
+});
+
+module.exports = app;
